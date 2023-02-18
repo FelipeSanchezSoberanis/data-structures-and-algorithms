20e07ba71db295ac429ff88cc2d6c6fdd42e0700
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-02-18 12:42:51 -0600

    Primitive calculator with history

--- programs/algorithmic-toolbox/dynamic-programming-1/PrimitiveCalculator.java
+++ programs/algorithmic-toolbox/dynamic-programming-1/PrimitiveCalculator.java
@@ -1,14 +1,15 @@
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PrimitiveCalculator {
     private static final Logger LOGGER = Logger.getLogger(PrimitiveCalculator.class.getName());
+    private static final Level LOGGER_LEVEL = Level.OFF;
 
-    private static int primitiveCalculator(int number) {
+    private static int[] primitiveCalculator(int number) {
         int[] operations = new int[number + 1];
+        String[] connections = new String[number + 1];
 
         for (int i = 0; i < operations.length; i++) {
             if (i == 0 || i == 1) {
@@ -20,7 +21,16 @@ public class PrimitiveCalculator {
             int option2 = i % 2 == 0 ? operations[i / 2] + 1 : Integer.MAX_VALUE;
             int option3 = i - 1 > 0 ? operations[i - 1] + 1 : Integer.MAX_VALUE;
 
-            operations[i] = Collections.min(List.of(option1, option2, option3));
+            if (option1 < option2 && option1 < option3) {
+                operations[i] = option1;
+                connections[i] = "x3";
+            } else if (option2 < option3) {
+                operations[i] = option2;
+                connections[i] = "x2";
+            } else {
+                operations[i] = option3;
+                connections[i] = "+1";
+            }
 
             LOGGER.log(Level.INFO, "i: {0}", i);
             LOGGER.log(Level.INFO, "Option 1: {0}", option1);
@@ -29,19 +39,46 @@ public class PrimitiveCalculator {
             LOGGER.log(Level.INFO, "Min option: {0}", operations[i]);
         }
 
-        return operations[number];
+        int noOperations = operations[number];
+        int[] numbers = new int[noOperations + 1];
+        int currentNumber = number;
+        int counter = noOperations - 1;
+
+        numbers[noOperations] = currentNumber;
+
+        while (currentNumber != 1) {
+            String connection = connections[currentNumber];
+
+            switch (connection) {
+                case "x3":
+                    currentNumber /= 3;
+                    break;
+                case "x2":
+                    currentNumber /= 2;
+                    break;
+                case "+1":
+                    currentNumber--;
+                    break;
+            }
+
+            numbers[counter] = currentNumber;
+            counter--;
+        }
+
+        return numbers;
     }
 
     public static void main(String[] args) {
-        LOGGER.setLevel(Level.INFO);
+        LOGGER.setLevel(LOGGER_LEVEL);
 
         Scanner scanner = new Scanner(System.in);
 
         int number = scanner.nextInt();
 
-        int noOperations = primitiveCalculator(number);
+        int[] operations = primitiveCalculator(number);
 
-        System.out.println(noOperations);
+        System.out.println(operations.length - 1);
+        Arrays.stream(operations).forEach(o -> System.out.format("%s ", o));
 
         scanner.close();
     }
