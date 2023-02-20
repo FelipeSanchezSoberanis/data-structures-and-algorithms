e7d35fe5dcaae4b0a030ea79099b2169039decea
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-02-20 02:38:38 -0600

    Completely different approach

--- programs/algorithmic-toolbox/dynamic-programming-1/LCS2.java
+++ programs/algorithmic-toolbox/dynamic-programming-1/LCS2.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -8,122 +5,6 @@ import java.util.logging.Logger;
 public class LCS2 {
     private static final Logger LOGGER = Logger.getLogger(LCS2.class.getName());
     private static final Level LOGGER_LEVEL = Level.OFF;
-
-    private static int getNoAlignedNumbers(
-            List<String> alignedString1, List<String> alignedString2) {
-        int result = 0;
-        for (int i = 0; i < alignedString1.size(); i++) {
-            if (alignedString1.get(i).equals(alignedString2.get(i))) result++;
-        }
-        return result;
-    }
-
-    private static List<List<String>> getAlignmentFromMatrix(
-            int[][] distanceMatrix, int[] A, int[] B) {
-        List<String> alignedString1 = new ArrayList<String>();
-        List<String> alignedString2 = new ArrayList<String>();
-
-        outputAlignment(
-                A,
-                B,
-                distanceMatrix,
-                distanceMatrix.length - 1,
-                distanceMatrix[distanceMatrix.length - 1].length - 1,
-                alignedString1,
-                alignedString2);
-
-        List<List<String>> alignedStrings = new ArrayList<>();
-        alignedStrings.add(alignedString1);
-        alignedStrings.add(alignedString2);
-        return alignedStrings;
-    }
-
-    private static void outputAlignment(
-            int[] A,
-            int[] B,
-            int[][] D,
-            int i,
-            int j,
-            List<String> alignedString1,
-            List<String> alignedString2) {
-        if (i == 0 && j == 0) return;
-
-        if (i > 0 && D[i][j] == D[i - 1][j] + 1) {
-            outputAlignment(A, B, D, i - 1, j, alignedString1, alignedString2);
-
-            alignedString1.add(Integer.toString(A[i - 1]));
-            alignedString2.add("-");
-
-        } else if (j > 0 && D[i][j] == D[i][j - 1] + 1) {
-            outputAlignment(A, B, D, i, j - 1, alignedString1, alignedString2);
-
-            alignedString1.add("-");
-            alignedString2.add(Integer.toString(B[j - 1]));
-        } else {
-            outputAlignment(A, B, D, i - 1, j - 1, alignedString1, alignedString2);
-
-            alignedString1.add(Integer.toString(A[i - 1]));
-            alignedString2.add(Integer.toString(B[j - 1]));
-        }
-
-        LOGGER.info(
-                String.format(
-                        "String1: %s    String2: %s",
-                        alignedString1.toString(), alignedString2.toString()));
-    }
-
-    private static int[][] editDistanceMatrix(int[] A, int[] B) {
-        int ALength = A.length + 1;
-        int BLength = B.length + 1;
-
-        int[][] D = new int[ALength][BLength];
-
-        for (int i = 0; i < ALength; i++) {
-            for (int j = 0; j < BLength; j++) {
-                LOGGER.info(String.format("i: %s    j: %s", i, j));
-
-                if (j == 0) {
-                    D[i][0] = i;
-                    continue;
-                } else if (i == 0) {
-                    D[0][j] = j;
-                    continue;
-                }
-
-                int insertion = D[i][j - 1] + 1;
-                int deletion = D[i - 1][j] + 1;
-
-                if (A[i - 1] == B[j - 1]) {
-                    int match = D[i - 1][j - 1];
-                    int[] options = {insertion, deletion, match};
-                    D[i][j] = Arrays.stream(options).min().getAsInt();
-
-                    LOGGER.info(String.format("options: %s", Arrays.toString(options)));
-                    LOGGER.info(String.format("min: %s", D[i][j]));
-                } else {
-                    int mismatch = D[i - 1][j - 1] + 1;
-                    int[] options = {insertion, deletion, mismatch};
-                    D[i][j] = Arrays.stream(options).min().getAsInt();
-
-                    LOGGER.info(String.format("options: %s", Arrays.toString(options)));
-                    LOGGER.info(String.format("min: %s", D[i][j]));
-                }
-            }
-        }
-
-        if (LOGGER_LEVEL.equals(Level.INFO)) {
-            System.out.println("=====");
-            System.out.println("Matrix:");
-            for (int i = 0; i < ALength; i++) {
-                for (int j = 0; j < BLength; j++) {
-                    System.out.format("[%s]", D[i][j]);
-                }
-                System.out.println();
-            }
-        }
-
-        return D;
-    }
 
     public static void main(String[] args) {
         LOGGER.setLevel(LOGGER_LEVEL);
@@ -144,29 +25,31 @@ public class LCS2 {
 
         scanner.close();
 
-        int[][] distanceMatrix = editDistanceMatrix(numberArray1, numberArray2);
-        List<List<String>> alignedStrings =
-                getAlignmentFromMatrix(distanceMatrix, numberArray1, numberArray2);
-        List<String> alignedString1 = alignedStrings.get(0);
-        List<String> alignedString2 = alignedStrings.get(1);
-        int result = getNoAlignedNumbers(alignedString1, alignedString2);
+        int result = lcs(numberArray1, numberArray2);
 
-        if (LOGGER_LEVEL.equals(Level.INFO)) {
-            System.out.println("=====");
-            System.out.println("Aligned words:");
-            for (String c : alignedString1) {
-                System.out.format("%s ", c);
-            }
-            System.out.println();
-            for (String c : alignedString2) {
-                System.out.format("%s ", c);
-            }
-            System.out.println();
-        }
-
-        if (LOGGER_LEVEL.equals(Level.INFO)) {
-            System.out.print("=====\nResult: ");
-        }
         System.out.println(result);
+    }
+
+    private static int lcs(int[] numberArray1, int[] numberArray2) {
+        int matrix[][] = new int[numberArray1.length + 1][numberArray2.length + 1];
+
+        for (int i = 0; i <= numberArray1.length; i++) {
+            matrix[i][numberArray2.length] = 0;
+        }
+        for (int i = 0; i <= numberArray2.length; i++) {
+            matrix[numberArray1.length][i] = 0;
+        }
+
+        for (int i = numberArray1.length - 1; i >= 0; i--) {
+            for (int j = numberArray2.length - 1; j >= 0; j--) {
+                if (numberArray1[i] == numberArray2[j]) {
+                    matrix[i][j] = matrix[i + 1][j + 1] + 1;
+                } else {
+                    matrix[i][j] = Math.max(matrix[i][j + 1], matrix[i + 1][j]);
+                }
+            }
+        }
+
+        return matrix[0][0];
     }
 }
