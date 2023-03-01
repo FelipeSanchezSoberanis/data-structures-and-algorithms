ced2c99739cfb368dfcd9dac5f4b11ce8381413c
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-02-28 18:30:54 -0600

    So close

--- data-structures/basic-data-structures/NetworkSimulation.java
+++ data-structures/basic-data-structures/NetworkSimulation.java
@@ -1,15 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class NetworkSimulation {
     private static final Logger LOGGER = Logger.getLogger(NetworkSimulation.class.getName());
-    private static final Level LOGGER_LEVEL = Level.INFO;
+    private static final Level LOGGER_LEVEL = Level.OFF;
 
     private static class Packet {
         private Integer arrivalTime;
@@ -24,16 +26,8 @@ public class NetworkSimulation {
             return arrivalTime;
         }
 
-        public void setArrivalTime(int arrivalTime) {
-            this.arrivalTime = arrivalTime;
-        }
-
         public Integer getProcessingTime() {
             return processingTime;
-        }
-
-        public void setProcessingTime(int processingTime) {
-            this.processingTime = processingTime;
         }
 
         @Override
@@ -46,61 +40,55 @@ public class NetworkSimulation {
         }
     }
 
-    private static int[] simulate(int bufferSize, Packet[] packets) {
-        int startSearchIndex = 0;
+    private static List<Integer> simulate(int bufferSize, Packet[] packets) {
+        List<Integer> simulationResults = new ArrayList<>(packets.length);
 
-        int[] simulationResult = new int[packets.length];
         Deque<Packet> packetsInBuffer = new LinkedList<>();
 
-        final int[] iWrapper = new int[1];
-
-        int counter = 0;
-        for (int i = 0; i <= packets[packets.length - 1].getArrivalTime(); i++) {
-            LOGGER.info(String.format("i: %s", i));
-
-            iWrapper[0] = i;
-
-            LOGGER.info(String.format("Bufer is empty? %s", packetsInBuffer.isEmpty()));
+        int processedTime = 0;
+        int processingTimeSum = 0;
+        int startSearchAt = 0;
+        int currentTime = 0;
+        while (simulationResults.size() < packets.length) {
             if (!packetsInBuffer.isEmpty()) {
-                LOGGER.info(
-                        String.format(
-                                "Arrival time + processing time:  %s",
-                                packetsInBuffer.peekFirst().getArrivalTime()
-                                        + packetsInBuffer.peekFirst().getProcessingTime()));
+                Packet firstPacket = packetsInBuffer.peekFirst();
+
+                if (firstPacket.getProcessingTime().equals(processedTime)) {
+                    processedTime = 0;
+                    packetsInBuffer.removeFirst();
+                    processingTimeSum -= firstPacket.getProcessingTime();
+                }
+            } else {
+                processedTime = 0;
             }
 
-            if (!packetsInBuffer.isEmpty()
-                    && packetsInBuffer.peekFirst().getArrivalTime()
-                                    + packetsInBuffer.peekFirst().getProcessingTime()
-                            <= i) {
-                LOGGER.info("Removing first element in buffer");
-                packetsInBuffer.removeFirst();
-            }
+            for (int j = startSearchAt; j < packets.length; j++) {
+                Packet packet = packets[j];
 
-            LOGGER.info(
-                    String.format(
-                            "Packets in buffer after striping: %s", packetsInBuffer.toString()));
-
-            for (int j = startSearchIndex; j < packets.length; j++) {
-                if (packets[j].getArrivalTime().equals(i)) {
-                    if (packetsInBuffer.size() < bufferSize) {
-                        simulationResult[counter] = i;
-                        packetsInBuffer.addLast(packets[j]);
-                    } else {
-                        simulationResult[counter] = -1;
-                    }
-
-                    counter++;
-                } else {
-                    startSearchIndex = j;
+                if (packet.getArrivalTime() > currentTime) {
+                    startSearchAt = j;
                     break;
+                }
+
+                if (packetsInBuffer.size() < bufferSize) {
+                    if (packet.getProcessingTime() > 0) packetsInBuffer.addLast(packet);
+                    simulationResults.add(currentTime + processingTimeSum - processedTime);
+                    processingTimeSum += packet.getProcessingTime();
+                } else {
+                    simulationResults.add(-1);
                 }
             }
 
+            processedTime++;
+            currentTime++;
+
+            LOGGER.info(String.format("Time: %s", currentTime - 1));
             LOGGER.info(String.format("Packets in buffer: %s", packetsInBuffer.toString()));
+            LOGGER.info(String.format("Processed time: %s", processedTime - 1));
+            LOGGER.info(String.format("Simulation results: %s", simulationResults.toString()));
         }
 
-        return simulationResult;
+        return simulationResults;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -108,8 +96,10 @@ public class NetworkSimulation {
 
         String testFilesDir =
                 "/home/felipe/Documents/uady/coursera/data-structures-statements-starters/week1_basic_data_structures/3_network_simulation/tests";
-        String testFile = "/16";
+        String testFile = "/20";
         Scanner scanner = new Scanner(new File(testFilesDir + testFile));
+
+        // Scanner scanner = new Scanner(System.in);
 
         int bufferSize = scanner.nextInt();
         int noPackets = scanner.nextInt();
@@ -125,8 +115,10 @@ public class NetworkSimulation {
         LOGGER.info(String.format("Number of packets: %s", noPackets));
         LOGGER.info(String.format("Packet array: %s", Arrays.toString(packets)));
 
-        int[] result = simulate(bufferSize, packets);
+        List<Integer> result = simulate(bufferSize, packets);
 
-        LOGGER.info(String.format("Result: %s", Arrays.toString(result)));
+        LOGGER.info(String.format("Result: %s", result.toString()));
+
+        result.forEach(p -> System.out.println(p));
     }
 }
