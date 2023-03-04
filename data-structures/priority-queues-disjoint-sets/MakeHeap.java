240b4caf20924efd86f7b8139323eda7e01bc5b2
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-03-04 13:22:25 -0600

    Base file

---
+++ data-structures/priority-queues-disjoint-sets/MakeHeap.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class MakeHeap {
+    private static final Logger LOGGER = Logger.getLogger(MakeHeap.class.getName());
+    private static final Level LOGGER_LEVEL = Level.INFO;
+
+    private static void configureLogger(Logger logger) {
+        logger.setLevel(LOGGER_LEVEL);
+
+        logger.setUseParentHandlers(false);
+
+        SimpleFormatter simpleFormatter =
+                new SimpleFormatter() {
+                    @Override
+                    public String format(LogRecord logRecord) {
+                        return String.format(
+                                "[%-7s] - %s \n", logRecord.getLevel(), logRecord.getMessage());
+                    }
+                };
+
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setFormatter(simpleFormatter);
+
+        logger.addHandler(consoleHandler);
+    }
+
+    public static void main(String[] args) {
+        configureLogger(LOGGER);
+
+        Scanner scanner = new Scanner(System.in);
+
+        int noNumbers = scanner.nextInt();
+        int[] numbers = new int[noNumbers];
+
+        for (int i = 0; i < noNumbers; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        LOGGER.info("Testing");
+    }
+}
