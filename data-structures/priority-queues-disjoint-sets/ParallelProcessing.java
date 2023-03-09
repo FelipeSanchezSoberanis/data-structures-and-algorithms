8ed04bd112a7eb0910088569c35d1baec805be01
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-03-09 10:14:34 -0600

    Implemented priority queue
    == Solution ==
    Priority queue that orders by value and index of worker

--- data-structures/priority-queues-disjoint-sets/ParallelProcessing.java
+++ data-structures/priority-queues-disjoint-sets/ParallelProcessing.java
@@ -35,19 +35,23 @@ public class ParallelProcessing {
     }
 
     private void assignJobs() {
-        // TODO: replace this code with a faster algorithm.
         assignedWorker = new int[jobs.length];
         startTime = new long[jobs.length];
         long[] nextFreeTime = new long[numWorkers];
+        CompleteBinaryTree tree = new CompleteBinaryTree(nextFreeTime);
         for (int i = 0; i < jobs.length; i++) {
             int duration = jobs[i];
-            int bestWorker = 0;
-            for (int j = 0; j < numWorkers; ++j) {
-                if (nextFreeTime[j] < nextFreeTime[bestWorker]) bestWorker = j;
-            }
-            assignedWorker[i] = bestWorker;
-            startTime[i] = nextFreeTime[bestWorker];
-            nextFreeTime[bestWorker] += duration;
+
+            DoubleValueNode topNode = tree.peekMin();
+
+            // int bestWorker = 0;
+            // for (int j = 0; j < numWorkers; ++j) {
+            //     if (nextFreeTime[j] < nextFreeTime[bestWorker]) bestWorker = j;
+            // }
+            assignedWorker[i] = topNode.getOriginalIndex();
+            startTime[i] = topNode.getValue();
+            tree.changePriority(0, topNode.getValue() + duration);
+            // nextFreeTime[bestWorker] += duration;
         }
     }
 
@@ -79,5 +83,105 @@ public class ParallelProcessing {
         public int nextInt() throws IOException {
             return Integer.parseInt(next());
         }
+    }
+}
+
+class CompleteBinaryTree {
+    private DoubleValueNode[] nodes;
+    private int size;
+
+    public CompleteBinaryTree(long[] nodes) {
+        this.nodes = new DoubleValueNode[nodes.length];
+        this.size = nodes.length;
+
+        for (int i = 0; i < this.size; i++) {
+            this.nodes[i] = new DoubleValueNode(i, nodes[i]);
+        }
+
+        buildHeap();
+    }
+
+    private void buildHeap() {
+        for (int i = Math.floorDiv(nodes.length, 2) - 1; i >= 0; i--) {
+            siftDown(i);
+        }
+    }
+
+    private void siftDown(int i) {
+        int minIndex = i;
+        int leftIndex = 2 * i + 1;
+        int rightIndex = 2 * i + 2;
+
+        if (rightIndex < size && nodes[rightIndex].getValue() < nodes[minIndex].getValue()) {
+            minIndex = rightIndex;
+        } else if (rightIndex < size
+                && nodes[rightIndex].getValue() == nodes[minIndex].getValue()
+                && nodes[rightIndex].getOriginalIndex() < nodes[minIndex].getOriginalIndex()) {
+            minIndex = rightIndex;
+        }
+
+        if (leftIndex < size && nodes[leftIndex].getValue() < nodes[minIndex].getValue()) {
+            minIndex = leftIndex;
+        } else if (leftIndex < size
+                && nodes[leftIndex].getValue() == nodes[minIndex].getValue()
+                && nodes[leftIndex].getOriginalIndex() < nodes[minIndex].getOriginalIndex()) {
+            minIndex = leftIndex;
+        }
+
+        if (minIndex != i) {
+            swap(i, minIndex);
+            siftDown(minIndex);
+        }
+    }
+
+    public void swap(int i, int j) {
+        DoubleValueNode value = nodes[i];
+        nodes[i] = nodes[j];
+        nodes[j] = value;
+    }
+
+    public DoubleValueNode extractMin() {
+        DoubleValueNode min = nodes[0];
+
+        swap(0, size - 1);
+        size--;
+        siftDown(0);
+
+        return min;
+    }
+
+    public DoubleValueNode peekMin() {
+        return nodes[0];
+    }
+
+    public void changePriority(int i, long newValue) {
+        nodes[i].setValue(newValue);
+        siftDown(i);
+    }
+}
+
+class DoubleValueNode {
+    private int originalIndex;
+    private long value;
+
+    public DoubleValueNode(int originalIndex, long value) {
+        this.originalIndex = originalIndex;
+        this.value = value;
+    }
+
+    public int getOriginalIndex() {
+        return originalIndex;
+    }
+
+    public void setOriginalIndex(int originalIndex) {
+        this.originalIndex = originalIndex;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
     }
 }
