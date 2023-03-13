2671d00e475cace311612a64bd16b178d0ed18b1
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-03-13 11:20:01 -0600

    Starter file

---
+++ data-structures/binary-search-trees/TreeOrders.java
@@ -0,0 +1,96 @@
+import java.io.*;
+import java.util.*;
+
+public class TreeOrders {
+
+    public static void main(String[] args) throws IOException {
+        new Thread(
+                        null,
+                        new Runnable() {
+                            public void run() {
+                                try {
+                                    new TreeOrders().run();
+                                } catch (IOException e) {
+                                }
+                            }
+                        },
+                        "1",
+                        1 << 26)
+                .start();
+    }
+
+    public void print(List<Integer> x) {
+        for (Integer a : x) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+    }
+
+    public void run() throws IOException {
+        TreeOrdersInner tree = new TreeOrdersInner();
+        tree.read();
+        print(tree.inOrder());
+        print(tree.preOrder());
+        print(tree.postOrder());
+    }
+}
+
+class TreeOrdersInner {
+    int n;
+    int[] key, left, right;
+
+    void read() throws IOException {
+        FastScanner in = new FastScanner();
+        n = in.nextInt();
+        key = new int[n];
+        left = new int[n];
+        right = new int[n];
+        for (int i = 0; i < n; i++) {
+            key[i] = in.nextInt();
+            left[i] = in.nextInt();
+            right[i] = in.nextInt();
+        }
+    }
+
+    List<Integer> inOrder() {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        // Finish the implementation
+        // You may need to add a new recursive method to do that
+
+        return result;
+    }
+
+    List<Integer> preOrder() {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        // Finish the implementation
+        // You may need to add a new recursive method to do that
+
+        return result;
+    }
+
+    List<Integer> postOrder() {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        // Finish the implementation
+        // You may need to add a new recursive method to do that
+
+        return result;
+    }
+}
+
+class FastScanner {
+    StringTokenizer tok = new StringTokenizer("");
+    BufferedReader in;
+
+    FastScanner() {
+        in = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() throws IOException {
+        while (!tok.hasMoreElements()) tok = new StringTokenizer(in.readLine());
+        return tok.nextToken();
+    }
+
+    int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+}
