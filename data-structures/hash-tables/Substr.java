5adfe0a5891dd09421c679a325714a8c6235e36a
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-03-12 22:24:39 -0600

    This no work

--- data-structures/hash-tables/Substr.java
+++ data-structures/hash-tables/Substr.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.Random;
 import java.util.StringTokenizer;
 
 public class Substr {
@@ -27,13 +28,40 @@ public class Substr {
 
 class Solver {
     private String s;
+    private long m1;
+    private long m2;
+    private long x;
+    private long[] h1;
+    private long[] h2;
 
     public Solver(String s) {
         this.s = s;
+        this.m1 = 1000000000L + 7L;
+        this.m2 = 1000000000L + 9L;
+        this.x = new Random(-1).nextLong(1000000000L) + 1;
+
+        precomputeHashes();
+    }
+
+    private void precomputeHashes() {
+        h1 = new long[s.length()];
+        h2 = new long[s.length()];
+
+        h1[0] = 0;
+        h2[0] = 0;
+
+        for (int i = 1; i < h1.length; i++) {
+            h1[i] = (x * h1[i - 1] + s.charAt(i)) % m1;
+            h2[i] = (x * h2[i - 1] + s.charAt(i)) % m2;
+        }
+    }
+
+    private long H(long[] h, int a, int l) {
+        return h[a + l - 1] - x * h[a];
     }
 
     public boolean ask(int a, int b, int l) {
-        return s.substring(a, a + l).equals(s.substring(b, b + l));
+        return H(h1, a, l) % m1 == H(h2, b, l) % m1 && H(h1, a, l) % m2 == H(h2, b, l) % m2;
     }
 }
 
