09400b587f863a54eda6c495fe5f4b3e38d6dcb0
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-03-13 16:49:20 -0600

    Stack with max value

--- data-structures/basic-data-structures/StackWithMax.java
+++ data-structures/basic-data-structures/StackWithMax.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -28,16 +27,24 @@ public class StackWithMax {
         FastScanner scanner = new FastScanner();
         int queries = scanner.nextInt();
         Stack<Integer> stack = new Stack<Integer>();
+        Stack<Integer> trackingStack = new Stack<Integer>();
 
         for (int qi = 0; qi < queries; ++qi) {
             String operation = scanner.next();
             if ("push".equals(operation)) {
                 int value = scanner.nextInt();
                 stack.push(value);
+
+                if (trackingStack.isEmpty() || value >= trackingStack.peek())
+                    trackingStack.push(value);
+
             } else if ("pop".equals(operation)) {
-                stack.pop();
+                int removedValue = stack.pop();
+
+                if (removedValue == trackingStack.peek()) trackingStack.pop();
+
             } else if ("max".equals(operation)) {
-                System.out.println(Collections.max(stack));
+                System.out.println(trackingStack.peek());
             }
         }
     }
