32357be3a6b5786454410fd0aa6b3b8d022570be
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-03-16 09:52:57 -0600

    StronglyConnected
    == Solution ==
    Kosaraju’s algorithm.

--- algorithms-on-graphs/decomposition-of-graphs-2/StronglyConnected.java
+++ algorithms-on-graphs/decomposition-of-graphs-2/StronglyConnected.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
@@ -8,26 +10,6 @@ import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class StronglyConnected {
-    private static MyLogger LOGGER;
-
-    private static int numStronglyConnectedComponents() {
-        return 1;
-    }
-
-    private static List<List<Integer>> reverseAdjacentList(List<List<Integer>> adj) {
-        List<List<Integer>> adjReversed = new ArrayList<>();
-
-        for (int i = 0; i < adj.size(); i++) adjReversed.add(new ArrayList<>());
-
-        for (int u = 0; u < adj.size(); u++) {
-            for (int v : adj.get(u)) {
-                adjReversed.get(v).add(u);
-            }
-        }
-
-        return adjReversed;
-    }
-
     private static List<List<Integer>> readData() {
         Scanner scanner = new Scanner(System.in);
 
@@ -51,17 +33,88 @@ public class StronglyConnected {
     }
 
     public static void main(String[] args) {
-        LOGGER = new MyLogger(StronglyConnected.class.getName());
-
         List<List<Integer>> adj = readData();
+        Graph graph = new Graph(adj);
 
-        LOGGER.infoFormat("Adjacent list: %s", adj.toString());
+        System.out.println(graph.getNumStronglyConnComp());
+    }
+}
+
+class Graph {
+    private final MyLogger LOGGER;
+
+    private List<List<Integer>> adj;
+
+    public Graph(List<List<Integer>> adj) {
+        this.LOGGER = new MyLogger(Graph.class.getName());
+
+        this.adj = adj;
+    }
+
+    public int getNumStronglyConnComp() {
+        Stack<Integer> vertexStack = depthFirstSearch();
+        LOGGER.infoFormat("Vertex stack: %s", vertexStack.toString());
 
         List<List<Integer>> adjReversed = reverseAdjacentList(adj);
 
-        LOGGER.infoFormat("Reversed adjacent list: %s", adjReversed.toString());
+        boolean[] isVisited = new boolean[adjReversed.size()];
+        Arrays.fill(isVisited, false);
 
-        System.out.println(numStronglyConnectedComponents());
+        int strongConnCompCounter = 0;
+        while (!vertexStack.isEmpty()) {
+            int topVertex = vertexStack.pop();
+            if (!isVisited[topVertex]) {
+                explore(topVertex, isVisited, adjReversed);
+                strongConnCompCounter++;
+            }
+        }
+
+        return strongConnCompCounter;
+    }
+
+    private Stack<Integer> depthFirstSearch() {
+        Stack<Integer> stack = new Stack<>();
+
+        boolean[] isVisited = new boolean[adj.size()];
+        Arrays.fill(isVisited, false);
+
+        for (int i = 0; i < adj.size(); i++) {
+            if (!isVisited[i]) explore(i, isVisited, stack);
+        }
+
+        return stack;
+    }
+
+    private void explore(int u, boolean[] isVisited, List<List<Integer>> adjReversed) {
+        isVisited[u] = true;
+
+        for (int v : adjReversed.get(u)) {
+            if (!isVisited[v]) explore(v, isVisited, adjReversed);
+        }
+    }
+
+    private void explore(int u, boolean[] isVisited, Stack<Integer> stack) {
+        isVisited[u] = true;
+
+        for (int v : adj.get(u)) {
+            if (!isVisited[v]) explore(v, isVisited, stack);
+        }
+
+        stack.push(u);
+    }
+
+    private List<List<Integer>> reverseAdjacentList(List<List<Integer>> adj) {
+        List<List<Integer>> adjReversed = new ArrayList<>();
+
+        for (int i = 0; i < adj.size(); i++) adjReversed.add(new ArrayList<>());
+
+        for (int u = 0; u < adj.size(); u++) {
+            for (int v : adj.get(u)) {
+                adjReversed.get(v).add(u);
+            }
+        }
+
+        return adjReversed;
     }
 }
 
@@ -71,7 +124,7 @@ class MyLogger {
 
     public MyLogger(String className) {
         this.logger = Logger.getLogger(className);
-        this.loggerLevel = Level.INFO;
+        this.loggerLevel = Level.OFF;
 
         configureLogger();
     }
