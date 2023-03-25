3f70bedaa797754ce124b8743aff71a22fcc4f76
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-03-25 14:48:06 -0600

    Vertex class

--- algorithms-on-graphs/minimum-spanning-trees/ConnectingPoints.java
+++ algorithms-on-graphs/minimum-spanning-trees/ConnectingPoints.java
@@ -29,6 +29,64 @@ public class ConnectingPoints {
     }
 }
 
+class Vertex {
+    private static MyLogger LOGGER = new MyLogger(Vertex.class.getName()).disabled();
+
+    private int x;
+    private int y;
+
+    public static double getDistance(Vertex a, Vertex b) {
+        LOGGER.infoFormat("getDistance(Vertex a, Vertex b)");
+        LOGGER.infoFormat("Vertex a: %s", a);
+        LOGGER.infoFormat("Vertex b: %s", b);
+
+        double diff1 = a.getX() - b.getX();
+        double diff2 = a.getY() - b.getY();
+        double distance = Math.sqrt(Math.pow(diff1, 2.0) + Math.pow(diff2, 2.0));
+
+        LOGGER.infoFormat("Distance: %s", distance);
+
+        return distance;
+    }
+
+    public Vertex(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Vertex other = (Vertex) obj;
+        if (x != other.x) return false;
+        if (y != other.y) return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex [x=" + x + ", y=" + y + "]";
+    }
+}
 class DataReader {
     private int[] x;
     private int[] y;
